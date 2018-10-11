81482f664d48047ab8818183a490e4db2659e45a
RyanPerkins <bobefett.qwert12@gmail.com>
2018-10-11 08:47:08 -0600

    created controller, view, model, and resources, and filled model with colorChange method

--- ColorMagic/Controller/ColorChangeViewController.swift
+++ ColorMagic/Controller/ColorChangeViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+public class ColorChangeViewController: UIViewController
+{
+    private lazy var myColor : ColorTool = ColorTool()
+    
+    
+    public override func viewDidLoad()-> Void
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    public override func didReceiveMemoryWarning() -> Void
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
---
+++ ColorMagic/Model/ColorTool.swift
@@ -0,0 +1,21 @@
+//
+//  ColorTool.swift
+//  ColorMagic
+//
+//  Created by Perkins, Ryan on 10/11/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    public func createRandomColor() -> UIColor
+    {
+        let redPercent : CGFloat = CGFloat (Double((arc4random() % 256)) / 255.00)
+        let greenPercent : CGFloat = CGFloat (Double((arc4random() % 256)) / 255.00)
+        let bluePercent : CGFloat = CGFloat (Double((arc4random() % 256)) / 255.00)
+        return UIColor(red:redPercent, green:greenPercent, blue:bluePercent, alpa:CGFloat(100))
+    }
+}
